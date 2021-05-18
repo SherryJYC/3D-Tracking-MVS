ac9df6430e2a4eacfcea03ca1bb542fee7da5797
Yuchang Jiang <yujiang@phys.ethz.ch>
2021-05-18 08:57:33 +0200

    add reid in matcher

--- src/runMCTracker.py
+++ src/runMCTracker.py
@@ -16,6 +16,7 @@ import argparse
 import os
 
 from mcTracker import Camera, Pitch
+from mcTracker_reid import Camera_reid, Pitch_reid
 
 def config():
     a = argparse.ArgumentParser()
@@ -25,18 +26,30 @@ def config():
     a.add_argument("--input_cam2", help="track result of cam2", default='data/tracks/right_filtered_team_pitch.txt')
     a.add_argument("--output_file", help="output file name", default='cam1_right_team.txt')
 
+    a.add_argument('--doreid', action='store_true', help='whether to use reid')
+
     args = a.parse_args()
     return args
 
 
 def main(args):
-    # load camera
-    cam1 = Camera(args.input_cam1, 'cam1')
-    cam2 = Camera(args.input_cam2, 'right')
+    if args.doreid:
+        # load camera
+        cam1 = Camera_reid(args.input_cam1, 'cam1')
+        cam2 = Camera_reid(args.input_cam2, 'right')
 
-    # create pitch
-    output_file = os.path.join(args.input_cam1[:-len(os.path.basename(args.input_cam1))], args.output_file)
-    pitch = Pitch(output=output_file)
+        # create pitch
+        output_file = os.path.join(args.input_cam1[:-len(os.path.basename(args.input_cam1))], args.output_file)
+        pitch = Pitch_reid(output=output_file)
+    else:
+        # load camera
+        cam1 = Camera(args.input_cam1, 'cam1')
+        cam2 = Camera(args.input_cam2, 'right')
+
+        # create pitch
+        output_file = os.path.join(args.input_cam1[:-len(os.path.basename(args.input_cam1))], args.output_file)
+        pitch = Pitch(output=output_file)
+
     pitch.add_cam(cam1)
     pitch.add_cam(cam2)
     print('find {} cameras in frame {} to {}'.format(len(pitch.cam_list), pitch.tstart, pitch.tend))
