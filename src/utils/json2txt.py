e23b2d10b362c4f6fa84eef4d75d123213388934
Tianyu Wu <wuti@phys.ethz.ch>
2021-05-20 11:37:31 +0200

    modify json2txt to convert ground truth

--- src/utils/json2txt.py
+++ src/utils/json2txt.py
@@ -5,40 +5,50 @@ Created on Thu Mar 18 16:43:40 2021
 
 @author: yujiang
 """
-"""
-
-convert label.json to txt file
-
-format:
-    <frame id>, <object id>, <x1>, <y1>, <w>, <h>, <x2>, <y2>
-
-"""
 import os 
 import json
 import numpy as np
 import argparse
-
-def config():
-    a = argparse.ArgumentParser()
-    a.add_argument("--jsonfile", help="path to json file", default='./data/json/annotations.json')
-    a.add_argument("--jsonfolder", help="path to json folder", default='./data/dataset5/cam4/json')
-    a.add_argument("--output", help="path to output text file", default='./data/dataset5/cam4/labels.txt')
-    a.add_argument("--muljson",  help="whether to combine multiple json file", action="store_true")
-    args = a.parse_args()    
-    return args
 
 def json2text(jsonfile):
     f = open(jsonfile,) 
     data = json.load(f)
     
     rows = []
-    objectkey = np.empty((3,))
+    objectkey = np.empty((70,))
     for obj in data['objects']:
         # get drone id: 1, 2, 3 => 0, 1, 2
-        droneidx = int(obj['classTitle'][-1]) - 1
+        # bbox -> 1-30
+        # goalkeep -> 31-32
+        # coach -> 41
+        # judge -> 51+
+        print(obj['classTitle'])
+        droneidx = 60
+        if 'bbox' in obj['classTitle']:
+            if obj['classTitle'] == 'bbox':
+                droneidx = 1
+            else:
+                droneidx = int(obj['classTitle'][4:])+1
+        if 'goalkeeper' in obj['classTitle']:
+            if obj['classTitle'] == 'goalkeeper':
+                droneidx = 31
+            else:
+                droneidx = int(obj['classTitle'][10:])+31
+        if 'coach' in obj['classTitle']:
+            if obj['classTitle'] == 'coach':
+                droneidx = 41
+            else:
+                droneidx = int(obj['classTitle'][5:])+41
+        if 'judge' in obj['classTitle']:
+            if obj['classTitle'] == 'judge':
+                droneidx = 51
+            else:
+                droneidx = int(obj['classTitle'][5:])+51
+        # droneidx = int(obj['classTitle'][-1]) - 1
         objectkey[droneidx] = obj['id']
 
     objectkey = objectkey.astype('int32').tolist()
+    print(objectkey)
 
     for frame in data['frames']:
         frameid = frame['index']
@@ -76,6 +86,7 @@ def muljson2text(json_folder):
             instances = frame['figures']
             for instance in instances:
                 points = instance['geometry']['points']['exterior']
+                print(objectkey)
                 w = points[1][0]-points[0][0]
                 h = points[1][1]-points[0][1]
                 objectid = objectkey.index(instance['objectId'])
@@ -87,14 +98,16 @@ def muljson2text(json_folder):
 
 if __name__=="__main__":
     
-    args = config()
+    a = argparse.ArgumentParser()
+    a.add_argument("--jsonfile", help="path to json file", default='./data/json/annotations.json')
+    a.add_argument("--jsonfolder", help="path to json folder", default='./data/dataset5/cam4/json')
+    a.add_argument("--output", help="path to output text file", default='./data/dataset5/cam4/labels.txt')
+    a.add_argument("--muljson", help="whether to combine multiple json file", default=True)
+    args = a.parse_args()    
     
-    if not os.path.exists(args.output[0:-len(args.output.split('/')[-1])]):
-        os.mkdir(args.output[0:-len(args.output.split('/')[-1])])
-    
-    if args.muljson==True:
-        print('mul json2text')
-        np.savetxt(args.output, np.array(muljson2text(args.jsonfolder)), delimiter=',')
-    else:
-        print('json2text')
-        np.savetxt(args.output, np.array(json2text(args.jsonfile)), delimiter=',')
+    # if args.muljson:
+    #     print('mul json2text')
+    #     np.savetxt(args.output, np.array(muljson2text(args.jsonfolder)), delimiter=',')
+    # else:
+    print('json2text')
+    np.savetxt(args.output, np.array(json2text(args.jsonfile)), delimiter=',')
